bd285d1321d517bc4bbd3710be23e168dedb399e
Domonique Taylor <taylord651@gmail.com>
2021-02-25 18:26:35 -0600

    Completed Enums

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -21,14 +21,19 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event() { }
+    private EventType type;
 
-    public Event(String name, String description, String contactEmail) {
+    public Event() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public Event(String name, String description, String contactEmail, EventType type) {
+        this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
-        this.id = nextId;
-        nextId++;
+        this.type = type;
     }
 
     public String getName() {
@@ -57,6 +62,14 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     @Override
